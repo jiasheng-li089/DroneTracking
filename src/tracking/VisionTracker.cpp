4f8731f70a34b687cb0b9eb08eed98e848f4ff51
Jiasheng Li <jiasheng.li.jsl@gmail.com>
2026-04-30 10:40:47 +1200

    feat: remove invalid source file and configuration

--- src/tracking/VisionTracker.cpp
+++ src/tracking/VisionTracker.cpp
@@ -10,6 +10,15 @@ VisionTracker::VisionTracker(QObject *parent) : QObject(parent) {}
 VisionTracker::~VisionTracker() = default;
 
 void VisionTracker::process_frames(const int camera_id, const std::string & serial, const rs2::frameset& frames) {
-    spdlog::debug("Processing frameset with {} frames", frames.size());
+    // detect the possible position of the target (aurcode) related to the camera.
+
+
+    // get the real distance of the target (aurcode) from the camera
+
+
+    // based on the real distance and the related position, calculate the relative position of the target (aurcode) to the camera
+
+
+    // average the relative positions of the target (aurcode) to the cameras to get a stable position estimation of the target (aurcode)
 }
 } // namespace tracking
