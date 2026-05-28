4efa250149a3649edd923d19d2695da485bb4003
Jason Lee <ljsalm089@gmail.com>
2026-05-28 23:47:21 +1200

    feat: add functions to read camera meta data

--- src/camera/camera_util.cpp
+++ src/camera/camera_util.cpp
@@ -2,19 +2,17 @@
 
 #include <opencv2/opencv.hpp>
 #include <spdlog/spdlog.h>
+#include <string>
 #include <sys/ioctl.h>
 
-#if defined(__linux__)
 #include <fcntl.h>
 #include <linux/videodev2.h>
 #include <unistd.h>
-#else
-#endif
+#include <vector>
 
 std::vector<int> detect_rgb_cameras() {
   std::vector<int> rgb_cameras;
 
-#if defined(__linux__)
   for (int i = 0; i < 30; i++) {
     std::string camera_path = "/dev/video" + std::to_string(i);
     int fd = open(camera_path.c_str(), O_RDWR);
@@ -34,19 +32,52 @@ std::vector<int> detect_rgb_cameras() {
 
     close(fd);
   }
-#else
-  // for macOS and Windows, fallback: brute force open and check for 3 channels
-  for (int i = 0; i < 10; i++) {
-    cv::VideoCapture cap(i);
-    if (cap.isOpened()) {
-      cv::Mat frame;
-      cap >> frame;
-      if (!frame.empty() && frame.channels() == 3) {
-        rgb_cameras.push_back(i);
-      }
-    }
-  }
-#endif
 
-  return rgb_cameras;
+  return std::move(rgb_cameras);
+}
+
+CameraMeta get_camera_meta(int camera_id) {
+  std::string camera_path = "/dev/video" + std::to_string(camera_id);
+  int fd = open(camera_path.c_str(), O_RDONLY);
+  if (-1 == fd) {
+    return CameraMeta{-1, camera_id, "", {}};
+  }
+
+  struct v4l2_frmsizeenum frmsize;
+  frmsize.index = 0;
+
+  frmsize.pixel_format = V4L2_PIX_FMT_YUYV;
+
+  std::vector<CameraResolution> supported_resolutions;
+  while (ioctl(fd, VIDIOC_ENUM_FRAMESIZES, &frmsize) == 0) {
+    if (frmsize.type == V4L2_FRMSIZE_TYPE_DISCRETE) {
+      struct v4l2_frmivalenum frmival = {};
+      frmival.index = 0;
+      frmival.pixel_format = frmsize.pixel_format;
+      frmival.width = frmsize.discrete.width;
+      frmival.height = frmsize.discrete.height;
+
+      CameraResolution tmp {
+        static_cast<int>(frmsize.discrete.width), 
+        static_cast<int>(frmsize.discrete.height),
+        {}
+      };
+      while (ioctl(fd, V4L2_FRMIVAL_TYPE_DISCRETE) == 0) {
+        if (frmival.type == V4L2_FRMIVAL_TYPE_DISCRETE) {
+          double fps = static_cast<double>(frmival.discrete.denominator) /
+                       static_cast<double>(frmival.discrete.numerator);
+          tmp.fps.push_back(fps);
+        }
+        frmival.index ++;
+      }
+
+      supported_resolutions.push_back(tmp);
+    }
+    frmsize.index ++;
+  }
+  close(fd);
+
+  // read serial from camera
+
+  return CameraMeta{-1, camera_id, "", {}};
 }
--- src/camera/camera_util.h
+++ src/camera/camera_util.h
@@ -1,5 +1,25 @@
 #pragma once
 
 #include <vector>
+#include <string>
+
+struct CameraResolution {
+    int width;
+    int height;
+    std::vector<double> fps;
+};
+
+struct CameraMeta {
+    int result;
+    int id;
+    std::string serial;
+    std::vector<CameraResolution> supported_resolutions;
+};
 
 std::vector<int> detect_rgb_cameras();
+
+
+CameraMeta get_camera_meta(int camera_id);
+
+
+std::vector<CameraMeta> get_camera_metas(std::vector<int> camera_ids);
