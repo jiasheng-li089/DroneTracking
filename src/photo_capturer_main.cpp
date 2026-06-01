edfcab81b2134ad81a3a028a171649614b365d03
Jason Lee <ljsalm089@gmail.com>
2026-06-01 16:11:01 +1200

    feat: automatically detect camera serial

--- src/photo_capturer_main.cpp
+++ src/photo_capturer_main.cpp
@@ -1,40 +1,69 @@
+#include "camera/camera_util.h"
 #include "gui/AutoPhotoCaptureWindow.h"
 #include "logger.h"
 
 #include <QApplication>
-#include <QSurfaceFormat> // Required for configuring OpenGL Widget
 #include <QDir>
+#include <QSurfaceFormat> // Required for configuring OpenGL Widget
+#include <algorithm>
+#include <cctype>
+#include <iostream>
 #include <opencv2/core/types.hpp>
 #include <qdir.h>
-
+#include <string>
 
 int main(int argc, char *argv[]) {
-    Logger::init("logs/photo_capturing.log");
-    // QSurfaceFormat must be set BEFORE creating the QApplication instance.
-    QSurfaceFormat format;
-    format.setVersion(3, 3);
-    format.setProfile(QSurfaceFormat::CoreProfile);
-    QSurfaceFormat::setDefaultFormat(format);
+  Logger::init("logs/photo_capturing.log");
+  // QSurfaceFormat must be set BEFORE creating the QApplication instance.
+  QSurfaceFormat format;
+  format.setVersion(3, 3);
+  format.setProfile(QSurfaceFormat::CoreProfile);
+  QSurfaceFormat::setDefaultFormat(format);
 
-    QApplication a(argc, argv);
+  QApplication a(argc, argv);
 
-    QDir current = QDir::current();
-    if (!current.exists("photos")) {
-        current.mkdir("photos");
+  QDir current = QDir::current();
+  if (!current.exists("photos")) {
+    current.mkdir("photos");
+  }
+  current.cd("photos");
+
+  auto camera_metas = get_all_camera_metas();
+  std::string camera_serial = "";
+
+  for (const auto &meta : camera_metas) {
+    auto serial = meta.serial;
+    std::transform(serial.begin(), serial.end(), serial.begin(), ::tolower);
+    auto non_zero_index = serial.find_first_not_of('0');
+    if (non_zero_index != std::string::npos) {
+      serial = serial.substr(non_zero_index);
     }
-    current.cd("photos"); 
 
-    auto camera_serial = "903223052137";
-    AutoPhotoCaptureWindowConfig config {
-        camera_serial,
-        cv::Size(10, 7),
-        25.0f,
-        current.absolutePath().toStdString(),
-        camera_serial
-    };
+    if (meta.supported_resolutions.size() <= 0 ||
+        meta.supported_resolutions[0].fps.size() <= 0) {
+      continue;
+    }
 
-    AutoPhotoCaptureWindow w(config, nullptr);
-    w.show();
+    if (!camera_serial.empty()) {
+        std::cerr << "Too many cameras are installed into the device" <<std::endl;
+        return -1;
+    }
+    camera_serial = serial;
+  }
 
-    return a.exec();
+  if (camera_serial.empty()) {
+    std::cerr << "Failed to find a camera" << std::endl;
+    return -1;
+  } else {
+    std::cout << "Start photo capturing with camera #" << camera_serial << std::endl;
+  }
+
+  AutoPhotoCaptureWindowConfig config{camera_serial, cv::Size(10, 7), 25.0f,
+                                      current.absolutePath().toStdString(),
+                                      camera_serial};
+
+  AutoPhotoCaptureWindow w(config, nullptr);
+  w.show();
+
+  return a.exec();
 }
